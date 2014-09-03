7a093520e95c7f8667ffb652a9368576c290dd0d
Bob Jiang <dashaomai@gmail.com>
2014-09-03 13:52:35 +0800

    将通用功能封装到 beej/common.h 当中

---
+++ beej/common.h
@@ -0,0 +1,18 @@
+/**
+ * common.h -- the common function(s) for beej's tutoral
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+// get sockaddr, IPv4 or IPv6:
+void * get_in_addr(struct sockaddr *sa) {
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in *)sa)->sin_addr);
+  }
+
+  return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
