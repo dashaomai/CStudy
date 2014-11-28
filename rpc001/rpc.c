3f5152d4468040c5854db5e0587cc5a1fb8b8fcf
Bob Jiang <dashaomai@gmail.com>
2014-11-28 11:50:24 +0800

    初步建立 rpc 的启动机制，目前还没有接收逻辑

--- rpc001/rpc.c
+++ rpc001/rpc.c
@@ -12,11 +12,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include <sys/errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
-#include "st.h"
+#include <st.h>
 
 void start_processes(const int procnum);
 void create_rpc_listeners(void);
+void accept_client(void);
+
+static void *handle_clientconn(void *arg);
 
 void err_sys_quit(const int fd, const char *fmt, ...);
 static void err_doit(int fd, int errnoflag, const char *fmt, va_list ap);
@@ -27,18 +34,20 @@ char *err_tstamp(void);
 
 int main(int argc, const char *argv[])
 {
-  if (st_init() < 0)
-    err_sys_quit(0, "st_init");
-
   start_processes(5);
 
   create_rpc_listeners();
+
+  accept_client();
 
   return 0;
 }
 
 int   vp_count;
-int   *fd_list;
+
+st_netfd_t   *fd_list;
+st_netfd_t   rpc_fd;
+
 int   my_index;
 pid_t my_pid;
 
@@ -71,15 +80,144 @@ void start_processes(const int procnum)
     } else {
       // parent, or master
       vp_count = i;
-      fd_list = (int *)calloc(i, sizeof(int));
     }
+    fd_list = (st_netfd_t*)calloc(i, sizeof(st_netfd_t));
   }
 }
 
+/**
+ * 创建用于 rpc 通讯的 TCP/IP 内容
+ */
 void create_rpc_listeners(void) {
   fprintf(stderr, "my index: %d\tmy pid: %d\n", my_index, my_pid);
 
-  for (;;);
+  int fd;
+  struct addrinfo hints, *ai, *p;
+
+  int reuseaddr = 1; // for SO_REUSEADDR
+  int rv;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = PF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  int rpc_port = RPC_PORT + my_index;
+  char port[16];
+
+  sprintf(port, "%d", rpc_port);
+
+  if ((rv = getaddrinfo("0.0.0.0", port, &hints, &ai)) != 0) {
+    err_sys_quit(1, "getaddrinfo");
+  }
+
+  for (p = ai; p != NULL; p = p->ai_next) {
+    fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+    if (fd < 0) continue;
+
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(int));
+
+    if (bind(fd, p->ai_addr, p->ai_addrlen) < 0) {
+      close(fd);
+      continue;
+    }
+
+    break;
+  }
+
+  if (p == NULL) {
+    err_sys_quit(1, "failed to bind at 0.0.0.0\n");
+  }
+
+  freeaddrinfo(ai); // all done with this
+  // 防止野指针
+  ai = NULL;
+  p = NULL;
+
+  if (listen(fd, 10) == -1) {
+    err_sys_quit(1, "failed to listen in %d\n", rpc_port);
+  }
+
+  // 初始化 st 线程
+  if (st_init() < 0) {
+    err_sys_quit(1, "failed to init st\n");
+  }
+
+  // 转换 fd
+  if ((rpc_fd = st_netfd_open(fd)) == NULL)
+    err_sys_quit(1, "failed to convert fd into st_fd: %s\n", strerror(errno));
+
+  fd_list[my_index] = rpc_fd;
+}
+
+/**
+ * 接收 rpc 连接
+ */
+void accept_client(void) {
+  st_netfd_t client;
+  struct sockaddr from;
+  int fromlen = sizeof(from);
+
+  while ((client = st_accept(rpc_fd, &from, &fromlen, ST_UTIME_NO_TIMEOUT)) != NULL) {
+    st_netfd_setspecific(client, get_in_addr(&from), NULL);
+
+    if (st_thread_create(handle_clientconn, &client, 0, 0) == NULL)
+      fprintf(stderr, "failed to create the client thread: %s\n", strerror(errno));
+  }
+
+  st_netfd_close(rpc_fd);
+
+  return 0;
+}
+
+static void *handle_clientconn(void *arg) {
+  st_netfd_t client = *(st_netfd_t*)arg;
+  arg = NULL;
+
+  // 握手
+  // rpc客户端连入后，会主动发来客户端自己的 index
+  // 长度为 1 字节
+  char    buf[1024];
+  size_t  len;
+
+  // 先只读取 1 字节的客户端握手头，表示客户端自己的 index
+  if ((len = st_read(client, buf, 1, ST_UTIME_NO_TIMEOUT)) < 0) {
+    fprintf(stderr, "failed to handshake from client: %s\n", strerror(errno));
+    goto close_fd_and_quit;
+  } else if (len == 0) {
+    goto close_fd_and_quit;
+  }
+
+  uint8_t   client_index = (uint8_t)buf[0];
+
+  fprintf(stdout, "来自 rpc 客户端 #%d 的握手已经成功建立\n", client_index);
+
+  // 如果 client_index 等于自己的 my_index，则这个有问题
+  if (client_index == my_index) {
+    fprintf(stderr, "rpc client promet the same index with mine.\n");
+    goto close_fd_and_quit;
+  }
+
+  // 将客户端 fd 放入属于它 index 的 fd_list 内
+  if (fd_list[client_index] != NULL) {
+    fprintf(stderr, "This client #%d has connected before, replace it.\n", client_index);
+    st_netfd_close(fd_list[client_index]);
+  }
+  fd_list[client_index] = client;
+
+  // 循环服务处理
+  for (;;) {
+    if ((len = st_read(client, buf, sizeof(buf), ST_UTIME_NO_TIMEOUT)) < 0) {
+      fprintf(stderr, "failed when read from client #%d.\n", client_index);
+      goto close_fd_and_quit;
+    } else if (len == 0) {
+      goto close_fd_and_quit;
+    }
+  }
+
+close_fd_and_quit:
+  st_netfd_close(client);
+  return 0;
 }
 
 /**
@@ -90,7 +228,7 @@ void err_sys_quit(const int fd, const char *fmt, ...)
   va_list ap;
 
   va_start(ap, fmt);
-  err_doit(stderr, 1, fmt, ap);
+  err_doit(1, 1, fmt, ap);
   va_end(ap);
   exit(1);
 }
