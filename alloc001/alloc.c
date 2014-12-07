9d82d8fbff952dd7bdc8825be7baaf00b5a2448f
Bob Jiang <dashaomai@gmail.com>
2014-12-08 04:33:55 +0800

    Add the union test.

--- alloc001/alloc.c
+++ alloc001/alloc.c
@@ -6,6 +6,11 @@ struct peer_info {
 	char	name[64];
 };
 
+union endin_info {
+	short	number;
+	char	bytes[2];
+};
+
 int main(int argc, char **argv) {
 	struct peer_info *list;
 
@@ -15,9 +20,13 @@ int main(int argc, char **argv) {
 	fprintf(stdout, "The size of struct is: %d bytes.\n", sizeof(struct peer_info));
 	fprintf(stdout, "The size of instance is: %d bytes.\n", sizeof(list[1]));
 
-	fprintf(stdout, "The address of instance(s) is: 0x%x\t0x%x\t0x%x.\n", &list[0], &list[1], &list[2]);
+	fprintf(stdout, "The address of instance(s) is: %d\t%d\t%d.\n", (int)&list[0], (int)&list[1], (int)&list[2]);
 
 	free(list);
+
+	union endin_info ei;
+	ei.number = 0xAABB;
+	fprintf(stdout, "The content of bytes: 0x%x 0x%x.\n", ei.bytes[0], ei.bytes[1]);
 
 	return 0;
 }
