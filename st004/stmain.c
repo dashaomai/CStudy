891f1e8459d2cbba5eb7482fc60341ad89fb899c
Bob Jiang <dashaomai@gmail.com>
2014-12-20 13:18:17 +0800

    Minor adjust the logic of va_list usaging.

--- st004/stmain.c
+++ st004/stmain.c
@@ -4,16 +4,23 @@
 #include <stdarg.h>
 #include <st.h>
 
-void *thread_main(void *arg);
-int add_all(int v1, ...);
+static void *thread_main(void *arg);
+static int add_all(int v1, ...);
 
 int main(int argc, const char *argv[])
 {
+  st_set_eventsys(ST_EVENTSYS_ALT);
+
   st_init();
 
   printf("entering main.\n");
 
   st_thread_create(thread_main, NULL, 0, 0);
+
+  int sum;
+  sum = add_all(8, 9, 10, 11, 12);
+
+  printf("add_all(8, 9, 10, 11, 12) = %d\n", sum);
 
   st_sleep(1);
 
@@ -22,7 +29,7 @@ int main(int argc, const char *argv[])
 
 void *thread_main(void *arg) {
   // this line should cause Error on FreeBSD 10.1 (X64 or Amd64)
-  printf("entering the state-threads.\n");
+  printf("entering the state-threads #%d.\n", 2);
 
   int sum;
   sum = add_all(1,2,3,4,5,6,7);
@@ -33,11 +40,12 @@ void *thread_main(void *arg) {
 int add_all(int v1, ...) {
   va_list ap;
 
-  int i, sum;
+  int i, j, sum;
   sum = 0;
 
   va_start(ap, v1);
-  for (i = v1; i != -1; i = va_arg(ap, int)) {
+  for (i = v1, j = 0; j <= 4; i = va_arg(ap, int), j++) {
+    printf("i = %d, sum = %d\n", i, sum);
     sum += i;
   }
   va_end(ap);
