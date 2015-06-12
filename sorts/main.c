f919a830dab7967921f0ca1dca9ffbff031a8b30
Bob Jiang <dashaomai@gmail.com>
2015-06-13 06:01:21 +0800

    将数组改为 5 倍大，并将生成的随机数放入 5 个数组内，以便直接形成 5 个完全相同的随机数组

--- sorts/main.c
+++ sorts/main.c
@@ -3,18 +3,37 @@
 #include <time.h>
 #include <stdint.h>
 
-#define LEN 100
+#define LEN 5
+#define COUNT 5
 
 int main(int argc, const char *argv[])
 {
   uint16_t *arr;
+  int size = sizeof(uint16_t) * LEN;
+  uint16_t *arr1, *arr2, *arr3, *arr4, *arr5;
+  uint16_t v;
 
-  arr = (uint16_t*)malloc(sizeof(uint16_t) * LEN);
+  arr = (uint16_t*)malloc(size * COUNT);
+
+  arr1 = arr;
+
+  arr2 = arr1 + size;
+  arr3 = arr2 + size;
+  arr4 = arr3 + size;
+  arr5 = arr4 + size;
+
   srand(time(0));
 
   for (int i = 1; i<=LEN; i++) {
-    arr[i] = rand();
-    printf("\t%u", arr[i]);
+    v = rand();
+
+    arr1[i] = v;
+    arr2[i] = v;
+    arr3[i] = v;
+    arr4[i] = v;
+    arr5[i] = v;
+
+    printf("\t%u", v);
 
     if (i % 10 == 0) {
       printf("\n");
@@ -22,7 +41,8 @@ int main(int argc, const char *argv[])
   }
   printf("\n");
 
-  free(arr), arr = NULL;
+  free(arr); arr = NULL;
+  arr1 = arr2 = arr3 = arr4 = arr5 = NULL;
 
   return 0;
 }
